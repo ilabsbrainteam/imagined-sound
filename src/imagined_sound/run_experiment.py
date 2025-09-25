773a8aaa0310a32996a479494b23a2518b606f8a
Daniel McCloy <dan@mccloy.info>
2025-09-25 14:57:15 -0500

    run multiple blocks

--- src/imagined_sound/run_experiment.py
+++ src/imagined_sound/run_experiment.py
@@ -4,7 +4,6 @@
 import yaml
 
 from pathlib import Path
-from types import SimpleNamespace
 
 import numpy as np
 
@@ -14,9 +13,9 @@ from expyfun.visual import FixationDot
 
 # set timing parameters
 block_start_delay = 0.4
-max_response_dur = 4.0
+imagine_dur = 3.5
 # pre_response_delay = 0.8  # defined differently per block
-inter_trial_interval = 1.2
+inter_trial_interval = 1.0
 
 # random number generator
 rng = np.random.default_rng(seed=8675309)
@@ -24,15 +23,12 @@ rng = np.random.default_rng(seed=8675309)
 # load textual prompts for the participant
 with open("prompts.yaml") as fid:
     prompts = yaml.safe_load(fid)
-prompts = SimpleNamespace(
-    **{k: " ".join(v.strip().split("\n")) for k, v in prompts.items()}
-)
+prompts = {k: " ".join(v.strip().split("\n")) for k, v in prompts.items()}
 paktc = " Press any key to continue."
 
 # load stimulus lists
-with open("blocks.yaml") as fid:
-    blocks = yaml.safe_load(fid)
-blocks = SimpleNamespace(**blocks)
+with open("block_stims.yaml") as fid:
+    block_stims = yaml.safe_load(fid)
 
 # colors
 colors = dict(
@@ -44,9 +40,14 @@ color_text = {
     name: f"{{color {col}}}{name}{{color {white}}}" for name, col in colors.items()
 }
 for col, text in color_text.items():
-    prompts.only_click = prompts.only_click.replace(col, text)
-    prompts.imagine_click_some = prompts.imagine_click_some.replace(col, text)
-    prompts.imagine_click_all = prompts.imagine_click_all.replace(col, text)
+    prompts["click_some"] = prompts["click_some"].replace(col, text)
+    prompts["imagine_click_some"] = prompts["imagine_click_some"].replace(col, text)
+    prompts["imagine_click_all"] = prompts["imagine_click_all"].replace(col, text)
+
+# gather up all the bits that differ between blocks
+blocks = {k: dict(prompt=v) for k, v in prompts.items() if k != "welcome"}
+_ = [blocks[k].update(stims=v) for k, v in block_stims.items() if k in blocks]
+_ = [blocks[k].update(click_all=(k.endswith("click_all"))) for k in blocks]
 
 with ExperimentController(
     "imagined-sound",
@@ -54,6 +55,7 @@ with ExperimentController(
     session="999",
     stim_fs=44100,
     stim_rms=0.01,
+    check_rms=None,
     output_dir="logs",
     version="dev",
 ) as ec:
@@ -62,71 +64,81 @@ with ExperimentController(
     radius = dot._circles[0]._radius
 
     # welcome instructions
-    ec.screen_prompt(prompts.welcome)
+    ec.screen_prompt(prompts["welcome"])
 
-    # first block: only click (no imagining)
-    ec.screen_prompt(prompts.only_click + paktc)
-    ec.screen_prompt("Here we go!", max_wait=block_start_delay, live_keys=[])
-    pre_response_delay = 0.8  # seconds
+    # loop over blocks
+    for block_name, block in blocks.items():
+        ec.screen_prompt(block["prompt"] + paktc)
+        ec.screen_prompt("Here we go!", max_wait=block_start_delay, live_keys=[])
 
-    for ix, stim_fname in enumerate(blocks.only_click, start=1):
-        # is this a button-press trial or not?
-        expect_press = ix % 3 == 0
-        # load the audio file
-        data, fs = read_wav(Path("stimuli") / "NWF003" / stim_fname)
-        assert fs == 44100, "bad stimulus sampling frequency"
-        rms_data = 0.01 * data / rms(data)
-        ec.load_buffer(rms_data)
-        duration = rms_data.shape[-1] / fs
-        # light gray fixation dot during stimulus (TODO: might be confusing?)
-        dot.set_colors(["0.9", "k"])
-        dot.draw()
-        # start the trial
-        ec.identify_trial(ec_id=f"{stim_fname}", ttl_id=[0, 0])
-        t_stim_start = ec.start_stimulus()  # sends a 1-trigger; "sentence start"
-        ec.wait_secs(duration)
-        ec.stop()
-        ec.stamp_triggers([4, 8], wait_for_last=False)  # 4, 8 = audio over
-        # white fixation dot during prepare-for-response period
-        dot.set_colors(["w", "k"])
-        dot.draw()
-        _ = ec.flip()
-        # larger, colored fixation dot during response period
-        # convert pyglet RGBA (ints in [0 255]) to matplotlib RGBA (floats in [0 1])
-        color = colors["pink"] if expect_press else colors["yellow"]
-        dot_col = tuple(map(lambda x: x / 255, color))
-        dot.set_colors([dot_col, "k"])
-        dot.set_radius(2 * radius, idx=0, units="pix")
-        dot.draw()  # won't actually change until next flip
-        pre_response_jitter = rng.uniform(low=0.0, high=0.4)
-        ec.wait_secs(pre_response_delay + pre_response_jitter)
-        t_response_start = ec.flip()
-        # response period
-        ec.stamp_triggers([8, 4], wait_for_last=False)  # 8, 4 = begin response
-        press_kwargs = dict(max_wait=max_response_dur, relative_to=t_response_start)
-        if expect_press:
-            pressed, t_press = ec.wait_one_press(**press_kwargs)
-            t_response_end = t_press or ec.get_time()
-        else:
-            presses_and_timestamps = ec.wait_for_presses(**press_kwargs)
-            if presses_and_timestamps:
-                pressed, t_press = list(zip(*presses_and_timestamps))
+        for ix, stim_fname in enumerate(block["stims"], start=1):
+            # is this a button-press trial or not?
+            expect_press = True if block["click_all"] else ix % 3 == 0
+            # load the audio file
+            data, fs = read_wav(Path("stimuli") / "NWF003" / stim_fname)
+            assert fs == 44100, "bad stimulus sampling frequency"
+            rms_data = 0.01 * data / rms(data)
+            ec.load_buffer(rms_data)
+            stim_duration = rms_data.shape[-1] / fs
+            # light gray fixation dot during stimulus (TODO: might be confusing?)
+            dot.set_colors(["0.9", "k"])
+            dot.draw()
+            # start the trial
+            ec.identify_trial(ec_id=f"{stim_fname}", ttl_id=[0, 0])
+            t_stim_start = ec.start_stimulus()  # sends a 1-trigger; "sentence start"
+            ec.wait_secs(stim_duration)
+            ec.stop()
+            ec.stamp_triggers([4, 8], wait_for_last=False)  # 4, 8 = audio over
+            # white fixation dot during prepare-for-response period
+            dot.set_colors(["w", "k"])
+            dot.draw()
+            _ = ec.flip()
+            # larger, colored fixation dot during response period
+            # convert pyglet RGBA (ints in [0 255]) to matplotlib RGBA (floats in [0 1])
+            color = colors["pink"] if expect_press else colors["yellow"]
+            dot_col = tuple(map(lambda x: x / 255, color))
+            dot.set_colors([dot_col, "k"])
+            dot.set_radius(2 * radius, idx=0, units="pix")
+            dot.draw()  # won't actually change until next flip
+            # triage trial timing depending on which block we're in
+            if block_name.startswith("imagine"):
+                # cue comes quickly, then more time to imagine
+                pre_response_delay = 0.7
+                max_response_dur = stim_duration + 0.5
             else:
-                pressed, t_press = (), ()
-            t_response_end = ec.get_time()
-        ec.stamp_triggers([8, 8], wait_for_last=True)  # 8, 8 = end response
-        # restore dot color and radius
-        dot.set_radius(radius, idx=0, units="pix")
-        dot.set_colors(["w", "k"])
-        dot.draw()
-        _ = ec.flip()
-        # logging and end trial
-        msg = (
-            f"stimulus began at {t_stim_start}. "
-            f"Response period began at {t_response_start}, ended at {t_response_end} "
-            f"({t_response_end - t_response_start} s duration). "
-            f"Button press at {t_press or '<NONE>'}"
-        )
-        ec.write_data_line(msg)
-        ec.trial_ok()
-        ec.wait_secs(inter_trial_interval)
+                # cue comes slowly ("do nothing" period), then short buttonpress timeout
+                pre_response_delay = stim_duration
+                max_response_dur = 1.4
+
+            pre_response_delay += rng.uniform(low=0.0, high=0.3)  # 300ms jitter
+            ec.wait_secs(pre_response_delay)
+            t_response_start = ec.flip()
+            # response period
+            ec.stamp_triggers([8, 4], wait_for_last=False)  # 8, 4 = begin response
+            press_kwargs = dict(max_wait=max_response_dur, relative_to=t_response_start)
+            if expect_press:
+                pressed, t_press = ec.wait_one_press(**press_kwargs)
+                t_response_end = t_press or ec.get_time()
+            else:
+                presses_and_timestamps = ec.wait_for_presses(**press_kwargs)
+                if presses_and_timestamps:
+                    pressed, t_press = list(zip(*presses_and_timestamps))
+                else:
+                    pressed, t_press = (), ()
+                t_response_end = ec.get_time()
+            ec.stamp_triggers([8, 8], wait_for_last=True)  # 8, 8 = end response
+            # restore dot color and radius
+            dot.set_radius(radius, idx=0, units="pix")
+            dot.set_colors(["w", "k"])
+            dot.draw()
+            _ = ec.flip()
+            # logging and end trial
+            msg = (
+                f"stimulus began at {t_stim_start}. "
+                f"Response period began at {t_response_start}, ended at {t_response_end} "
+                f"({t_response_end - t_response_start} s duration). "
+                f"Button press at {t_press or '<NONE>'}"
+            )
+            ec.write_data_line(msg)
+            ec.trial_ok()
+            ec.wait_secs(inter_trial_interval)
