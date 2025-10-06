6215e225c7722c214179cc4d1bbf18bfc98cc514
Daniel McCloy <dan@mccloy.info>
2025-10-06 17:35:35 -0500

    round 1 feedback

--- src/imagined_sound/run_experiment.py
+++ src/imagined_sound/run_experiment.py
@@ -12,11 +12,11 @@ from expyfun.stimuli import read_wav, rms
 from expyfun.visual import FixationDot
 
 # set timing parameters
-block_start_delay = 0.4
-imagine_dur = 3.5
-feedback_dur = 0.2
-# pre_response_delay = 0.8  # defined differently per block
+block_start_delay = 0.5
+feedback_dur = 0.3
 inter_trial_interval = 1.0
+n_practice = 4
+resp_duration_multiplier = 1.5  # multiplied by stimulus duration to get max timeout
 
 # random number generator
 rng = np.random.default_rng(seed=8675309)
@@ -36,17 +36,18 @@ colors = dict(
     pink=(187, 85, 102, 255),
     yellow=(221, 170, 51, 255),
 )
-white = (255, 255, 255, 255)
-color_text = {
-    name: f"{{color {col}}}{name}{{color {white}}}" for name, col in colors.items()
-}
-for col, text in color_text.items():
-    prompts["click_some"] = prompts["click_some"].replace(col, text)
-    prompts["imagine_click_some"] = prompts["imagine_click_some"].replace(col, text)
-    prompts["imagine_click_all"] = prompts["imagine_click_all"].replace(col, text)
 
 # gather up all the bits that differ between blocks
-blocks = {k: dict(prompt=v) for k, v in prompts.items() if k != "welcome"}
+blocks = {
+    k: dict(prompt=v)
+    for k, v in prompts.items()
+    if k != "welcome" and not k.endswith("practice")
+}
+_ = [
+    blocks[k.removesuffix("_practice")].update(practice=v)
+    for k, v in prompts.items()
+    if k.endswith("practice")
+]
 _ = [blocks[k].update(stims=v) for k, v in block_stims.items() if k in blocks]
 _ = [blocks[k].update(click_all=(k.endswith("click_all"))) for k in blocks]
 
@@ -70,6 +71,11 @@ with ExperimentController(
     # loop over blocks
     for block_name, block in blocks.items():
         ec.screen_prompt(block["prompt"] + paktc)
+        ec.screen_prompt(
+            f"First let's do {n_practice} practice trials (with feedback). "
+            f"Remember, {block['practice']}{paktc}"
+        )
+        practice = True
         ec.screen_prompt("Here we go!", max_wait=block_start_delay, live_keys=[])
 
         for ix, stim_fname in enumerate(block["stims"], start=1):
@@ -81,19 +87,15 @@ with ExperimentController(
             rms_data = 0.01 * data / rms(data)
             ec.load_buffer(rms_data)
             stim_duration = rms_data.shape[-1] / fs
-            # light gray fixation dot during stimulus (TODO: might be confusing?)
-            dot.set_colors(["0.9", "k"])
             dot.draw()
+
             # start the trial
             ec.identify_trial(ec_id=f"{stim_fname}", ttl_id=[0, 0])
             t_stim_start = ec.start_stimulus()  # sends a 1-trigger; "sentence start"
             ec.wait_secs(stim_duration)
             ec.stop()
             ec.stamp_triggers([4, 8], wait_for_last=False)  # 4, 8 = audio over
-            # white fixation dot during prepare-for-response period
-            dot.set_colors(["w", "k"])
-            dot.draw()
-            _ = ec.flip()
+
             # larger, colored fixation dot during response period
             # convert pyglet RGBA (ints in [0 255]) to matplotlib RGBA (floats in [0 1])
             color = colors["pink"] if expect_press else colors["yellow"]
@@ -101,49 +103,68 @@ with ExperimentController(
             dot.set_colors([dot_col, "k"])
             dot.set_radius(2 * radius, idx=0, units="pix")
             dot.draw()  # won't actually change until next flip
+
             # triage trial timing depending on which block we're in
             if block_name.startswith("imagine"):
-                # cue comes quickly, then more time to imagine
-                pre_response_delay = 0.7
-                max_response_dur = stim_duration + 0.5
+                pre_response_delay = 0.4  # short delay, then long time to imagine
+                max_wait = stim_duration * resp_duration_multiplier
             else:
-                # cue comes slowly ("do nothing" period), then short buttonpress timeout
-                pre_response_delay = stim_duration
-                max_response_dur = 1.4
-
-            pre_response_delay += rng.uniform(low=0.0, high=0.3)  # 300ms jitter
+                pre_response_delay = stim_duration  # long time before buttonpress
+                max_wait = 1.5
+            pre_response_delay += rng.uniform(low=0.0, high=0.4)  # 400ms jitter
             ec.wait_secs(pre_response_delay)
+            # TODO get presses during pre-response delay, to make sure the "rest" period
+            # isn't contaminated by motor activity
             t_response_start = ec.flip()
+
             # response period
             ec.stamp_triggers([8, 4], wait_for_last=False)  # 8, 4 = begin response
             pressed, t_press = ec.wait_one_press(
-                max_wait=max_response_dur, relative_to=t_response_start
+                min_wait=0.2,  # shouldn't be anyone faster than that...
+                max_wait=max_wait,
+                relative_to=t_response_start,
             )
             t_response_end = t_press or ec.get_time()
             ec.stamp_triggers([8, 8], wait_for_last=True)  # 8, 8 = end response
+
             # feedback
-            if (expect_press and pressed) or not (expect_press or pressed):
-                feedback = "✔"
-                color = "w"
-            else:
-                feedback = "✘"
-                color = "k"
-            dot.draw()
-            ec.screen_text(feedback, color=color, font_name="DejaVu Sans", wrap=False)
-            _ = ec.flip()
-            ec.wait_secs(feedback_dur)
+            if practice:
+                if (expect_press and pressed) or not (expect_press or pressed):
+                    feedback = "✔"
+                    color = "w"
+                    dur = feedback_dur
+                else:
+                    feedback = "✘"
+                    color = "k"
+                    dur = feedback_dur + 0.5
+                    ec.screen_text(
+                        "too slow", pos=(0, -0.075), wrap=False, font_size=18
+                    )
+                dot.draw()
+                ec.screen_text(
+                    feedback, color=color, font_name="DejaVu Sans", wrap=False
+                )
+                _ = ec.flip()
+                ec.wait_secs(feedback_dur)
+            # transition from "practice" to "real"
+            if ix == n_practice:
+                ec.screen_prompt(
+                    "OK, done with practice, so no more feedback. "
+                    f"Remember, {block['practice']}{paktc}"
+                )
+                practice = False
+
             # restore dot color and radius
             dot.set_radius(radius, idx=0, units="pix")
             dot.set_colors(["w", "k"])
             dot.draw()
             _ = ec.flip()
+
             # logging and end trial
-            msg = (
-                f"stimulus began at {t_stim_start}. "
-                f"Response period began at {t_response_start}, ended at {t_response_end} "
-                f"({t_response_end - t_response_start} s duration). "
-                f"Button press at {t_press or '<NONE>'}"
-            )
-            ec.write_data_line(msg)
+            ec.write_data_line("stimulus", value=stim_fname, timestamp=t_stim_start)
+            ec.write_data_line("stimulus", value="duration", timestamp=stim_duration)
+            ec.write_data_line("response", value="start", timestamp=t_response_start)
+            ec.write_data_line("response", value="end", timestamp=t_response_end)
+            ec.write_data_line("response", value="press", timestamp=t_press or np.nan)
             ec.trial_ok()
             ec.wait_secs(inter_trial_interval)
